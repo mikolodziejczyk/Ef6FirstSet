19dcf9dc48924e8308719463f263e9976b1a7f21
mko <gogs@fake.local>
2019-08-20 12:10:00 +0200

    Initial

---
+++ Ef6FirstSet/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace Ef6FirstSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Ef6Practice1Entities context = new Ef6Practice1Entities())
+            {
+
+            }
+        }
+    }
+}
